6d26b619be757ca7d5fc4c7a1709ef5233ea5e1f
Ivan Goncharuk <goncharukivan.biz@gmail.com>
2025-01-07 23:55:29 -0500

    stupid change to hello.ts

--- frontend/pages/api/hello.ts
+++ frontend/pages/api/hello.ts
@@ -4,5 +4,5 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  res.status(200).json({ message: "Hello from the API!" });
+  res.status(200).json({ message: "HELLO! This is api handler message from frontend/pages/api/hello.ts" });
 }
